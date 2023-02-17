717b62fba94d8c367db6631282e586bd1373f1f0
avinash <avinash.smcs@gmail.com>
2023-02-17 21:34:46 +0530

    Hello1

--- TestProject/settings.py
+++ TestProject/settings.py
@@ -203,15 +203,10 @@ USE_TZ = True
 
 # ******************  Static   ******************
 
-
 STATIC_URL = '/static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/')
-
-]
 
 # ******************  Media   ******************
 MEDIA_URL = '/media/'
